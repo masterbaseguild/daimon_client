5cac5088fdd68ebf107be4ebb7f01ee8f5c32603
mbentity <dariobizzarri02@gmail.com>
2024-12-07 11:45:24 +0100

    fixed movement

--- Assets/Scripts/MainUser.cs
+++ Assets/Scripts/MainUser.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class MainUser : MonoBehaviour
 {
     Vector3 spawnPoint = new Vector3(100, 105, 100);
+    GameObject playerCamera;
+    float xRotation;
+    float yRotation;
 
     void Start()
     {
+        playerCamera = transform.GetChild(0).gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         transform.position = spawnPoint;
     }
     void Update()
@@ -18,6 +24,17 @@ public class MainUser : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift)) transform.Translate(Vector3.down * Time.deltaTime * 10);
         if (Input.GetKeyDown(KeyCode.T)) MainUdpClient.SendChatMessage("Hello World!");
         if (Input.GetKeyDown(KeyCode.Y)) MainUdpClient.LogGameState();
+
+        float mouseX = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * 2048;
+        float mouseY = Input.GetAxisRaw("Mouse X") * Time.deltaTime * 2048;
+
+        yRotation += mouseY;
+        xRotation -= mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerCamera.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 
     public static Vector3 GetPosition()
