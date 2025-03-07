c267548be85f7a148d508de86294d27c3ebaf4ff
mbentity <dariobizzarri02@gmail.com>
2025-03-07 18:46:34 +0100

    implemented new physics

--- Assets/Scripts/MainUser.cs
+++ Assets/Scripts/MainUser.cs
@@ -12,17 +12,27 @@ public class MainUser : MonoBehaviour
     Transform groundCheck;
     LayerMask ground;
 
-    Vector3 spawnPoint = new Vector3(80, 64, 94);
-    float range = 5f; // break and place block range
-    float moveSpeed = 7.5f;
-    float jumpHeight = 1f;
-    float groundDistance = 0.25f; // ground check distance
-    float gravityAcceleration = -25f;
-    float sens = 750f; // mouse sensitivity
-    float delayBetweenPresses = 0.25f; // grace period for detecting double presses
-    int lowestY = -100; // if the player falls below this y value, they will be teleported to spawn
-    float hookRange = 125f; // hook range
-    float hookPower = 25f;
+    // parameters
+    public Vector3 spawnPoint;
+    public float breakPlaceRange;
+    public float movementSpeed;
+    public float jumpPower;
+    public float jumpCooldown;
+    public float groundCheckDistance;
+    public float gravityAcceleration;
+    public float cameraSensitivity;
+    public float delayBetweenDoublePresses;
+    public int loopbackY;
+    public float hookRange;
+    public float hookPower;
+    public float groundDrag;
+    public float airDrag;
+    public float physicsMultiplier;
+    public float hookedSpeedMultiplier;
+    public float runningSpeedMultiplier;
+    public float flyingSpeedMultiplier;
+    public float fallingSpeedMultiplier;
+    public float sneakingSpeedMultiplier;
 
     // user abilities
     bool canFly = true;
@@ -30,6 +40,7 @@ public class MainUser : MonoBehaviour
     bool canPhase = true;
 
     // user state
+    bool isReadyToJump = true;
     bool isFlying = true;
     bool isRunning = false;
     bool isPhasing = false;
@@ -41,13 +52,12 @@ public class MainUser : MonoBehaviour
     bool pressedFirstTimeW = false;
     float lastPressedTimeW;
 
-    float xRotation; // camera x rotation
-    float yRotation; // body y rotation
-    float gravityVelocity = 0f;
-    float moveSpeedMultiplier = 1f;
+    float xRotation;
+    float yRotation;
+    float movementSpeedMultiplier;
+    Vector3 movement;
     Vector3 hookPosL;
     Vector3 hookPosR;
-    Vector3 move = Vector3.zero; // current movement vector
 
     // setup components and cursor, then enable the user controller
     public void Enable()
@@ -62,96 +72,132 @@ public class MainUser : MonoBehaviour
         isEnabled = true;
     }
 
-    // run physics logic at a fixed interval
-    void FixedUpdate()
-    {
-        if (!isEnabled) return;
-
-        // ground check
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, ground);
-
-        // movement
-        move = (transform.forward * move.z + transform.right * move.x + transform.up * move.y).normalized * moveSpeed * moveSpeedMultiplier;
-        move.y += gravityVelocity;
-        rigidBody.AddForce(move, ForceMode.VelocityChange);
-
-        // hook movement
-        Vector3 forceL = Vector3.zero;
-        Vector3 forceR = Vector3.zero;
-        if (isHookedL) {
-            Vector3 directionL = hookPosL - transform.position;
-            forceL = directionL.normalized * hookPower;
-        }
-        if (isHookedR) {
-            Vector3 directionR = hookPosR - transform.position;
-            forceR = directionR.normalized * hookPower;
-        }
-        rigidBody.AddForce(forceL, ForceMode.VelocityChange);
-        rigidBody.AddForce(forceR, ForceMode.VelocityChange);
-    }
-
     // run input logic every frame
     void Update()
     {
         if (!isEnabled) return;
 
-        gravityVelocity += gravityAcceleration * Time.deltaTime;
-        if (isGrounded || isFlying || isHookedL || isHookedR) gravityVelocity = 0f;
-        if (transform.position.y < lowestY) transform.position = spawnPoint;
-        if (isFlying && isGrounded && !isPhasing) isFlying = false;
+        HandleMovement();
+        HandleSpeedMultipliers();
+        HandleSpeedCap();
 
-        move = Vector3.zero;
-        moveSpeedMultiplier = 1f;
+        HandleGrounded();
+        HandleDrag();
+        HandleJump();
 
-        if (Input.GetKey(KeyCode.Space)&&isFlying) move.y += 1f;
-        if (Input.GetKey(KeyCode.LeftShift)&&isFlying) move.y += -1f;
-        if(Input.GetKey(KeyCode.Space)&&isGrounded) gravityVelocity = Mathf.Sqrt(jumpHeight * -2f * gravityAcceleration);
+        HandleRun();
+        HandleDoubleWRun();
 
-        checkDoublePressSpace();
-        checkDoublePressW();
+        HandleDoubleSpaceFlight();
+        HandlePhase();
 
-        if(Input.GetMouseButtonDown(0)) breakBlock();
-        if(Input.GetMouseButtonDown(1)) placeBlock();
+        HandleCamera();
+        HandleBreakPlace();
+        HandleHooks();
 
-        if(Input.GetKeyDown(KeyCode.LeftControl)&&canRun) isRunning = true;
-        if(Input.GetKeyUp(KeyCode.W)&&isRunning) isRunning = false;
-        if(isHookedL||isHookedR) moveSpeedMultiplier *= 2f;
-        if(isRunning) moveSpeedMultiplier *= 2f;
-        if(isFlying) moveSpeedMultiplier *= 2f;
-        if(Input.GetKey(KeyCode.LeftShift)&&!isFlying) moveSpeedMultiplier /= 2f;
-
-        if (Input.GetKey(KeyCode.W)) move.z += 1f;
-        if (Input.GetKey(KeyCode.S)) move.z += -1f;
-        if (Input.GetKey(KeyCode.A)) move.x += -1f;
-        if (Input.GetKey(KeyCode.D)) move.x += 1f;
-
-        if (Input.GetKeyDown(KeyCode.Q)) startHookL();
-        if (!Input.GetKey(KeyCode.Q)) isHookedL = false;
-        if (Input.GetKeyDown(KeyCode.E)) startHookR();
-        if (!Input.GetKey(KeyCode.E)) isHookedR = false;
-
-        if (Input.GetKeyDown(KeyCode.T)) MainUdpClient.SendChatMessage("Hello World!");
-        if (Input.GetKeyDown(KeyCode.Y)) MainUdpClient.LogGameState();
-        if (Input.GetKeyDown(KeyCode.U)&&canPhase) {
-            isPhasing = !isPhasing;
-            GetComponent<CapsuleCollider>().enabled = !isPhasing;
-            isFlying = true;
-        }
-
-        yRotation += Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens;
-        xRotation -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens;
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
-        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        HandleLoopback();
+        HandleDebug();
     }
 
-    // double press logic
-    private void checkDoublePressSpace()
+    // run physics logic at a fixed interval
+    void FixedUpdate()
+    {
+        if (!isEnabled) return;
+
+        MovementForce();
+        GravityForce();
+        HookForce();
+    }
+
+    private void HandleLoopback()
+    {
+        if (transform.position.y < loopbackY) transform.position = spawnPoint;
+    }
+
+    private void HandleGrounded()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckDistance, ground);
+        if (isFlying && isGrounded && !isPhasing) isFlying = false;
+    }
+
+    private void HandleMovement()
+    {
+        movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) movement.z += 1f;
+        if (Input.GetKey(KeyCode.S)) movement.z += -1f;
+        if (Input.GetKey(KeyCode.A)) movement.x += -1f;
+        if (Input.GetKey(KeyCode.D)) movement.x += 1f;
+        if (Input.GetKey(KeyCode.Space)&&isFlying) movement.y += 1f;
+        if (Input.GetKey(KeyCode.LeftShift)&&isFlying) movement.y += -1f;
+    }
+
+    private void HandleSpeedMultipliers()
+    {
+        movementSpeedMultiplier = 1f;
+        if(isHookedL||isHookedR) movementSpeedMultiplier *= hookedSpeedMultiplier;
+        if(isRunning) movementSpeedMultiplier *= runningSpeedMultiplier;
+        if(isFlying) movementSpeedMultiplier *= flyingSpeedMultiplier;
+        else if(!isGrounded) movementSpeedMultiplier *= fallingSpeedMultiplier;
+        if(Input.GetKey(KeyCode.LeftShift)&&!isFlying) movementSpeedMultiplier *= sneakingSpeedMultiplier;
+    }
+
+    private void HandleSpeedCap()
+    {
+        if(isFlying)
+        {
+            Vector3 flatVel = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, rigidBody.velocity.z);
+            if(flatVel.magnitude > movementSpeed * movementSpeedMultiplier * physicsMultiplier)
+            {
+                Vector3 newVel = flatVel.normalized * movementSpeed * movementSpeedMultiplier * physicsMultiplier;
+                rigidBody.velocity = new Vector3(newVel.x, newVel.y, newVel.z);
+            }
+        }
+        else
+        {
+            Vector3 flatVel = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
+            if(flatVel.magnitude > movementSpeed * movementSpeedMultiplier * physicsMultiplier)
+            {
+                Vector3 newVel = flatVel.normalized * movementSpeed * movementSpeedMultiplier * physicsMultiplier;
+                rigidBody.velocity = new Vector3(newVel.x, rigidBody.velocity.y, newVel.z);
+            }
+        }
+    }
+
+    private void HandleDrag()
+    {
+        if (isGrounded||isFlying) rigidBody.drag = groundDrag;
+        else rigidBody.drag = airDrag;
+    }
+
+    private void HandleJump()
+    {
+        if(Input.GetKey(KeyCode.Space)&&isReadyToJump&&isGrounded)
+        {
+            isReadyToJump = false;
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0f, rigidBody.velocity.z);
+            rigidBody.AddForce(Vector3.up * jumpPower * physicsMultiplier, ForceMode.Impulse);
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void ResetJump()
+    {
+        isReadyToJump = true;
+    }
+
+    private void HandleHooks()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) StartHookL();
+        if (!Input.GetKey(KeyCode.Q)) isHookedL = false;
+        if (Input.GetKeyDown(KeyCode.E)) StartHookR();
+        if (!Input.GetKey(KeyCode.E)) isHookedR = false;
+    }
+
+    private void HandleDoubleSpaceFlight()
     {
         if (Input.GetKeyDown(KeyCode.Space)&&!isPhasing)
         {
-            if (pressedFirstTimeSpace&&Time.time - lastPressedTimeSpace <= delayBetweenPresses)
+            if (pressedFirstTimeSpace&&Time.time - lastPressedTimeSpace <= delayBetweenDoublePresses)
             {
                 if(canFly&&!isFlying) isFlying = true;
                 else if(isFlying) isFlying = false;
@@ -161,14 +207,14 @@ public class MainUser : MonoBehaviour
     
             lastPressedTimeSpace = Time.time;
         }
-        if (pressedFirstTimeSpace && Time.time - lastPressedTimeSpace > delayBetweenPresses) pressedFirstTimeSpace = false;
+        if (pressedFirstTimeSpace && Time.time - lastPressedTimeSpace > delayBetweenDoublePresses) pressedFirstTimeSpace = false;
     }
 
-    private void checkDoublePressW()
+    private void HandleDoubleWRun()
     {
         if (canRun&&Input.GetKeyDown(KeyCode.W))
         {
-            if (pressedFirstTimeW&&Time.time - lastPressedTimeW <= delayBetweenPresses)
+            if (pressedFirstTimeW&&Time.time - lastPressedTimeW <= delayBetweenDoublePresses)
             {
                 isRunning = true;
                 pressedFirstTimeW = false;
@@ -177,33 +223,68 @@ public class MainUser : MonoBehaviour
     
             lastPressedTimeW = Time.time;
         }
-        if (canRun&&pressedFirstTimeW && Time.time - lastPressedTimeW > delayBetweenPresses) pressedFirstTimeW = false;
+        if (canRun&&pressedFirstTimeW && Time.time - lastPressedTimeW > delayBetweenDoublePresses) pressedFirstTimeW = false;
     }
 
-    // block placement and breaking logic
+    private void HandleBreakPlace()
+    {
+        if(Input.GetMouseButtonDown(0)) BreakBlock();
+        if(Input.GetMouseButtonDown(1)) PlaceBlock();
+    }
+
     // NOTE: not implemented for now
-    private void breakBlock()
+    private void BreakBlock()
     {
         RaycastHit hit;
-        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, breakPlaceRange))
         {
             Vector3Int placedBlockPos = Vector3Int.RoundToInt(hit.point - hit.normal/2);
             Debug.Log("Break: from "+playerCamera.transform.position+" to "+placedBlockPos+"");
         }
     }
 
-    private void placeBlock()
+    // NOTE: not implemented for now
+    private void PlaceBlock()
     {
         RaycastHit hit;
-        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
+        if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, breakPlaceRange))
         {
             Vector3Int placedBlockPos = Vector3Int.RoundToInt(hit.point + hit.normal/2);
             Debug.Log("Place: from "+playerCamera.transform.position+" to "+placedBlockPos+"");
         }
     }
 
-    // hook logic
-    private void startHookL()
+    private void HandleRun()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl)&&canRun) isRunning = true;
+        if(Input.GetKeyUp(KeyCode.W)&&isRunning) isRunning = false;
+    }
+
+    private void HandleDebug()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) MainUdpClient.SendChatMessage("Hello World!");
+        if (Input.GetKeyDown(KeyCode.Y)) MainUdpClient.LogGameState();
+    }
+
+    private void HandlePhase()
+    {
+        if (Input.GetKeyDown(KeyCode.U)&&canPhase) {
+            isPhasing = !isPhasing;
+            GetComponent<CapsuleCollider>().enabled = !isPhasing;
+            isFlying = true;
+        }
+    }
+
+    private void HandleCamera()
+    {
+        yRotation += Input.GetAxisRaw("Mouse X") * Time.deltaTime * cameraSensitivity;
+        xRotation -= Input.GetAxisRaw("Mouse Y") * Time.deltaTime * cameraSensitivity;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.rotation = Quaternion.Euler(0f, yRotation, 0f);
+        playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private void StartHookL()
     {
         RaycastHit hit;
         if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, hookRange))
@@ -213,7 +294,8 @@ public class MainUser : MonoBehaviour
             isHookedL = true;
         }
     }
-    private void startHookR()
+
+    private void StartHookR()
     {
         RaycastHit hit;
         if(Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, hookRange))
@@ -224,7 +306,39 @@ public class MainUser : MonoBehaviour
         }
     }
 
+    private void MovementForce()
+    {
+        Vector3 moveVector = (transform.forward * movement.z + transform.right * movement.x + transform.up * movement.y).normalized * movementSpeed * 10f * movementSpeedMultiplier * physicsMultiplier;
+        rigidBody.AddForce(moveVector, ForceMode.Force);
+    }
+
+    private void GravityForce()
+    {
+        if (!(isGrounded || isFlying || isHookedL || isHookedR))
+        {
+            Vector3 gravity = Vector3.up * gravityAcceleration * physicsMultiplier * Time.fixedDeltaTime;
+            rigidBody.AddForce(gravity, ForceMode.Acceleration);
+        }
+    }
+
+    private void HookForce()
+    {
+        Vector3 forceL = Vector3.zero;
+        Vector3 forceR = Vector3.zero;
+        if (isHookedL) {
+            Vector3 directionL = hookPosL - transform.position;
+            forceL = directionL.normalized * hookPower * physicsMultiplier;
+        }
+        if (isHookedR) {
+            Vector3 directionR = hookPosR - transform.position;
+            forceR = directionR.normalized * hookPower * physicsMultiplier;
+        }
+        rigidBody.AddForce(forceL, ForceMode.Force);
+        rigidBody.AddForce(forceR, ForceMode.Force);
+    }
+
     // public methods
+
     public static Vector3 GetPosition()
     {
         return GameObject.Find("MainUser").transform.position;
