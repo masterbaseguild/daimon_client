2c51067905a135a9abb99fd1f723e243fe3333b0
mbentity <dariobizzarri02@gmail.com>
2025-08-31 20:11:03 +0200

    (WIP) edit mode inventory

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -18,7 +18,7 @@ public class UI : MonoBehaviour
 
     private void Start()
     {
-        sensitivitySlider.value = mainUser.GetComponent<MainUser>().cameraSensitivity/1000f;
+        sensitivitySlider.value = mainUser.GetComponent<MainUser>().cameraSensitivity / 1000f;
         usernameInput.text = udpClient.GetUsername();
         ipInput.text = udpClient.GetAddress();
         playBtn.onClick.AddListener(() =>
@@ -31,6 +31,22 @@ public class UI : MonoBehaviour
         usernameInput.onValueChanged.AddListener(delegate { EditText(); });
         ipInput.onValueChanged.AddListener(delegate { EditAddressText(); });
         sensitivitySlider.onValueChanged.AddListener(delegate { EditSensitivity(); });
+
+        // center the menu rectangle, then set it to 50% of the screen height and width
+
+        RectTransform menuRect = menu.GetComponent<RectTransform>();
+        RectTransform canvasRect = menuRect.GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+        menuRect.sizeDelta = new Vector2(canvasRect.rect.width * 0.5f, canvasRect.rect.height * 0.5f);
+        menuRect.anchoredPosition = Vector2.zero;
+
+        // set the Items panel 40 units smaller than the menu
+        RectTransform itemsRect = menu.transform.Find("Items").GetComponent<RectTransform>();
+        itemsRect.sizeDelta = new Vector2(-40, -80);
+        itemsRect.anchoredPosition = new Vector2(0, -20);
+
+        // set the Slider rect 20 units under the menu panel
+        RectTransform sliderRect = sensitivitySlider.GetComponent<RectTransform>();
+        sliderRect.anchoredPosition = new Vector2(0, -40);
     }
 
     private void EditText()
